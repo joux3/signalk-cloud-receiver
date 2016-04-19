d213bdc1bc0cb98a6a8abc4836a3207d390a2026
Antti Risteli <antti@kiinnost.us>
2016-04-19 14:25:06 +0300

    Do some ACKing of received messages

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const express = require('express');
 const app = express();
 let expressWs = require('express-ws')(app); // eslint-disable-line no-unused-vars
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3005;
 
 app.ws('/signalk-input', (ws) => {
   ws.on('message', msg => {
     if (ws.__boatId) {
       console.log('got message from ' + ws.__boatId + ':', msg);
-      handleBoatMessage(ws.__boatId, msg);
+      handleBoatMessage(ws.__boatId, ws, msg);
     } else {
       ws.__boatId = msg;
       // TODO validate boat id against config
@@ -25,5 +25,22 @@ app.ws('/signalk-input', (ws) => {
 
 app.listen(port);
 
-function handleBoatMessage(boatId, msg) {
+function handleBoatMessage(boatId, ws, msg) {
+  var parsed = tryParseJSON(msg);
+  console.log("managed to parse")
+  if (!parsed || typeof parsed.msgId !== 'number') {
+    return;
+  }
+
+  ws.send(JSON.stringify({
+    "ACK": parsed.msgId
+  }))
+}
+
+function tryParseJSON(string) {
+  try {
+    return JSON.parse(string);
+  } catch (e) {
+    return null;
+  }
 }
