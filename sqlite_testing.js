deaae6899f0d7aa3775543abbdfe76eb0b7ffe47
Antti Risteli <antti@kiinnost.us>
2017-01-15 14:38:01 +0200

    Use entries + vessels tables

--- sqlite_testing.js
+++ sqlite_testing.js
@@ -9,7 +9,12 @@ const inputFile = process.argv[2] || (console.log('Input file needed!'), process
 
 const db = new sqlite3.Database('test_db_'+new Date().getTime()+'.sqlite');
 Promise.promisifyAll(db)
-db.execAsync('PRAGMA journal_mode=WAL; CREATE TABLE entries (time INTEGER, vessel TEXT, path TEXT, value TEXT);').then(() => {
+let initQuery = 'PRAGMA journal_mode=WAL;'
+initQuery += 'PRAGMA foreign_keys = ON;'
+initQuery += 'CREATE TABLE vessels (id INTEGER PRIMARY KEY, vessel TEXT UNIQUE);'
+initQuery += 'CREATE TABLE entries (time INTEGER, vessel_id INTEGER NOT NULL, path TEXT, value TEXT, FOREIGN KEY(vessel_id) REFERENCES vessels(id));'
+
+db.execAsync(initQuery).then(() => {
   console.log('Opening', inputFile)
 
   return new Promise(resolve => {
@@ -33,15 +38,20 @@ db.execAsync('PRAGMA journal_mode=WAL; CREATE TABLE entries (time INTEGER, vesse
   return Promise.map(objects, object => {
     const updates = deltaParser(object)
     return Promise.map(updates, update => {
-      return db.runAsync("INSERT INTO entries (time, vessel, path, value) VALUES ($time, $vessel, $path, $value)", {
-        $time: update.timestamp.getTime(),
-        $vessel: update.vessel,
-        $path: update.pathStr,
-        $value: JSON.stringify(update.value)
+      return db.runAsync("INSERT OR IGNORE INTO vessels (vessel) VALUES($vessel);", {
+        $vessel: update.vessel
+      }).then(() => {
+        return db.runAsync("INSERT INTO entries (time, vessel_id, path, value) VALUES ($time, (SELECT id FROM vessels WHERE vessel = $vessel), $path, $value)", {
+          $time: update.timestamp.getTime(),
+          $vessel: update.vessel,
+          $path: update.pathStr,
+          $value: JSON.stringify(update.value)
+        })
       })
     })
   }).then(() => {
     console.log("Saving to sqlite took", new Date() - start, "ms")
+    db.close()
   })
 })
 
