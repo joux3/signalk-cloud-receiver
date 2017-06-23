6c42c0b7483ca8297fd0b65c2d47bee8c81bbda1
Antti Risteli <antti.risteli@reaktor.fi>
2017-06-23 09:26:47 +0300

    WIP timeline

--- public/timeline.js
+++ public/timeline.js
@@ -23,6 +23,7 @@
     trackData.forEach(sample => {
       sample.time = new Date(sample.time)
     })
+    var timeSamples = trackData.map(sample => sample.time)
 
     var graphWidth = $('#timeline-graph').get(0).offsetWidth
     var graphHeight = 100
@@ -44,6 +45,20 @@
       .attr('class', 'axis')
       .attr('transform', 'translate(10,75)')
       .call(xAxis)
+
+    $('#timeline-graph svg').on('mousemove', function(ev) {
+      if (ev.clientX < 10 || ev.clientX > graphWidth - 10) {
+        mouseOut()
+        return
+      }
+      var onGraphX = ev.clientX - 10
+      const time = x.invert(onGraphX)
+      const sampleIndex = d3.bisectLeft(timeSamples, time)
+      console.log(sampleIndex)
+    })
+    $('#timeline-graph svg').on('mouseout', mouseOut)
+    function mouseOut() {
+    }
   }
 
   $('#close-track-timeline').click(function () {
