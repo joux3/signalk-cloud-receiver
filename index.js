31180b23b16363313ccb3c145c1a4ba3b10bdac0
Antti Risteli <antti.risteli@reaktor.fi>
2019-01-22 17:23:53 +0200

    Use greenlock-express, add package-lock

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 const port = 3005
 const express = require('express')
@@ -9,13 +9,15 @@ const util = require('./util')
 
 let server = null
 if (process.env.CERTIFICATE_EMAIL && process.env.CERTIFICATE_DOMAIN) {
-  server = require('letsencrypt-express').create({
-    server: process.env.CERTIFICATE_SERVER || 'staging',
-    email: process.env.CERTIFICATE_EMAIL,
-    agreeTos: true,
-    approveDomains: [process.env.CERTIFICATE_DOMAIN],
-    app
-  }).listen(80, 443)
+  server = require('greenlock-express')
+    .create({
+      server: process.env.CERTIFICATE_SERVER || 'staging',
+      email: process.env.CERTIFICATE_EMAIL,
+      agreeTos: true,
+      approveDomains: [process.env.CERTIFICATE_DOMAIN],
+      app
+    })
+    .listen(80, 443)
 } else {
   server = app.listen(port)
   util.doLog('Starting server at', port)
@@ -28,22 +30,23 @@ const bodyParser = require('body-parser')
 const deltaParser = require('./delta_parser')
 const db = require('./database')
 
-
 const PASSWORD = process.env.PASSWORD || 'testpw'
 if (!process.env.PASSWORD && process.env.NODE_ENV === 'production') {
-  console.log("PASSWORD not set! Stopping")
+  console.log('PASSWORD not set! Stopping')
   process.exit(1)
 }
 if (!process.env.COOKIE_SECRET && process.env.NODE_ENV === 'production') {
-  console.log("COOKIE_SECRET not set! Stopping")
+  console.log('COOKIE_SECRET not set! Stopping')
   process.exit(1)
 }
 
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cookieSession({
-  name: 'session',
-  keys: [process.env.COOKIE_SECRET || 'fallbackSecret']
-}))
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: [process.env.COOKIE_SECRET || 'fallbackSecret']
+  })
+)
 app.use(function(req, res, next) {
   if (req.url === '/signalk-input/.websocket') {
     next()
@@ -56,12 +59,12 @@ app.use(function(req, res, next) {
     res.redirect('/')
   } else {
     const wrongPw = req.method === 'POST' && req.body.password
-    const form = '<form method="post" action="/"><input type="password" name="password"><input type="submit" value="Login"></form>'
+    const form =
+      '<form method="post" action="/"><input type="password" name="password"><input type="submit" value="Login"></form>'
     res.type('html').end(form + (wrongPw ? 'Wrong password!' : ''))
   }
 })
 app.use(express.static('public'))
-
 
 const connectedClients = {}
 let clientId = 0
@@ -69,26 +72,44 @@ app.ws('/signalk-output', (ws, req) => {
   util.doLog('Client connected', req.ip)
   ws.__clientId = clientId++
   connectedClients[ws.__clientId] = ws
-  ws.send(JSON.stringify({type: 'displayNames', displayNames}))
-  ws.send(JSON.stringify({type: 'state', data: worldState}))
+  ws.send(JSON.stringify({ type: 'displayNames', displayNames }))
+  ws.send(JSON.stringify({ type: 'state', data: worldState }))
 
   ws.on('message', function(msg) {
     const parsed = util.tryParseJSON(msg)
     if (!parsed) {
       return
     }
-    if (parsed.type === 'requestTrack' && typeof parsed.vesselId === 'string' &&
-      (typeof parsed.date === 'string' || parsed.date === undefined)) {
-      db.getPositionsForDateOr10Minutes(parsed.vesselId, parsed.date).then(positions => {
-        const msg = {type: 'boatTrack', vesselId: parsed.vesselId, positions}
-        if (typeof parsed.date === 'string') {
-          msg.date = parsed.date
+    if (
+      parsed.type === 'requestTrack' &&
+      typeof parsed.vesselId === 'string' &&
+      (typeof parsed.date === 'string' || parsed.date === undefined)
+    ) {
+      db.getPositionsForDateOr10Minutes(parsed.vesselId, parsed.date).then(
+        positions => {
+          const msg = {
+            type: 'boatTrack',
+            vesselId: parsed.vesselId,
+            positions
+          }
+          if (typeof parsed.date === 'string') {
+            msg.date = parsed.date
+          }
+          ws.send(JSON.stringify(msg))
         }
-        ws.send(JSON.stringify(msg))
-      })
-    } else if (parsed.type === 'requestTrackDates' && typeof parsed.vesselId === 'string') {
+      )
+    } else if (
+      parsed.type === 'requestTrackDates' &&
+      typeof parsed.vesselId === 'string'
+    ) {
       db.getDatesWithPositions(parsed.vesselId).then(dates => {
-        ws.send(JSON.stringify({type: 'boatTrackDates', vesselId: parsed.vesselId, dates}))
+        ws.send(
+          JSON.stringify({
+            type: 'boatTrackDates',
+            vesselId: parsed.vesselId,
+            dates
+          })
+        )
       })
     }
   })
@@ -99,20 +120,20 @@ app.ws('/signalk-output', (ws, req) => {
   }
 })
 
-app.ws('/signalk-input', (ws) => {
-  ws.__boatId = Math.random();
-  util.doLog('boat ' + ws.__boatId + ' connected');
+app.ws('/signalk-input', ws => {
+  ws.__boatId = Math.random()
+  util.doLog('boat ' + ws.__boatId + ' connected')
 
   ws.on('message', msg => {
-    handleBoatMessage(ws.__boatId, ws, msg);
-  });
+    handleBoatMessage(ws.__boatId, ws, msg)
+  })
 
   ws.on('close', () => {
-    util.doLog('Boat ' + (ws.__boatId || '<unknown>') + ' disconnected');
+    util.doLog('Boat ' + (ws.__boatId || '<unknown>') + ' disconnected')
   })
 
   ws.on('error', () => {
-    util.doLog('Boat ' + (ws.__boatId || '<unknown>') + ' error');
+    util.doLog('Boat ' + (ws.__boatId || '<unknown>') + ' error')
   })
 })
 
@@ -125,7 +146,7 @@ function sendClientUpdate(pathStr, pathState) {
       timestamp: pathState.timestamp
     }
   })
-  Object.keys(connectedClients).forEach((clientId) => {
+  Object.keys(connectedClients).forEach(clientId => {
     connectedClients[clientId].send(messageStr)
   })
 }
@@ -142,7 +163,10 @@ let worldState = {}
 db.getLatest30SecondsPerVessel().then(updates => {
   updates.forEach(update => {
     const globalPathStr = update.vessel + '.' + update.path
-    if (new Date(_.get(worldState, globalPathStr + ".timestamp", 0)) >= update.time) {
+    if (
+      new Date(_.get(worldState, globalPathStr + '.timestamp', 0)) >=
+      update.time
+    ) {
       return
     }
     const pathState = {
@@ -159,7 +183,10 @@ function handleBoatMessage(boatId, ws, msg) {
   const updates = deltaParser(parsed)
   updates.forEach(update => {
     const globalPathStr = update.vessel + '.' + update.pathStr
-    if (new Date(_.get(worldState, globalPathStr + ".timestamp", 0)) >= update.timestamp) {
+    if (
+      new Date(_.get(worldState, globalPathStr + '.timestamp', 0)) >=
+      update.timestamp
+    ) {
       return
     }
     const pathState = {
@@ -171,20 +198,24 @@ function handleBoatMessage(boatId, ws, msg) {
   })
 
   const hasMsgId = parsed && typeof parsed.msgId === 'number'
-  Promise.map(updates, db.storeUpdate).then(() => {
-    if (hasMsgId) {
-      ws.send(JSON.stringify({
-        "ACK": parsed.msgId
-      }))
-    }
-  }).catch((error) => {
-    util.doLog('Error in storing', error)
-    if (hasMsgId) {
-      ws.send(JSON.stringify({
-        "ERRACK": parsed.msgId
-      }))
-    }
-  })
+  Promise.map(updates, db.storeUpdate)
+    .then(() => {
+      if (hasMsgId) {
+        ws.send(
+          JSON.stringify({
+            ACK: parsed.msgId
+          })
+        )
+      }
+    })
+    .catch(error => {
+      util.doLog('Error in storing', error)
+      if (hasMsgId) {
+        ws.send(
+          JSON.stringify({
+            ERRACK: parsed.msgId
+          })
+        )
+      }
+    })
 }
-
-
